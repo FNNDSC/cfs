4dda942564d6dcab5653617a77d25583f5827750
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-07-11 09:40:28 -0400

    Add bulk ops

--- commands/rm.py
+++ commands/rm.py
@@ -7,13 +7,14 @@ from    pfmisc      import  Colors
 from    commands    import  ls
 import  shutil, os
 import  pudb
-from    typing      import  Generator
+from    typing      import  Generator, Any
 import  pandas      as      pd
 
 # Instantiate a "core" object called Imp
 _Rm:core.Core = core.Core
 
-LS:ls._ls    = ls._ls()
+_ls:type        = ls._ls
+LS:_ls          = _ls()
 
 # Define some methods!
 def badpath_errorExit(this, path:str, type:str, fs:str="") -> None:
@@ -25,7 +26,7 @@ def badpath_errorExit(this, path:str, type:str, fs:str="") -> None:
                                f"Please make sure the {type} really exists."
         this.stderr(message, 2)
 
-def file_rm(this, src:Path) -> bool:
+def file_rm(this, src:Path, onobjectstorage:bool = False) -> bool:
     """
     Remove a single file <src>
 
@@ -36,7 +37,11 @@ def file_rm(this, src:Path) -> bool:
     Returns:
         bool: success on delete operation
     """
-    srcReal:Path    = this.cfs2fs(src)
+    srcReal:Path    = Path('')
+    if onobjectstorage:
+        srcReal     = this.cfs2ofs(src)
+    else:
+        srcReal     = this.cfs2fs(src)
     if not srcReal.is_file():
          this.error_exit(str(src), "source file", "cfs")
     try:
@@ -66,13 +71,14 @@ def file_deleteSymlinkedFile(file:Path, l_ref:list=[]) -> bool:
     df:pd.DataFrame = FILE.File(file).manifest.remove_entry('name', file.name)
     # Then, if the ref path (i.e. the source) is also passed,
     # remove mention of file from the original real source manifest
+    # pudb.set_trace()
     if len(l_ref):
         ref:Path            = l_ref[0]
-        fileRefs:str        = FILE.File(ref).refs
+        fileRefs:str        = FILE.File(*FILE.sourcePartition_resolve(ref)).refs
         l_refs:list[str]    = fileRefs.split(';')
         l_refs.remove(f'->{str(file)}')
         fileRefs            = ';'.join(l_refs)
-        FILE.File(ref).manifest.update_entry({'refs': fileRefs}, ref.name)
+        FILE.File(*FILE.sourcePartition_resolve(ref)).manifest.update_entry({'refs': fileRefs}, ref.name)
     return True
 
 def file_removeSymlinkedCopies(file:Path, l_ref:list=[]) -> int:
@@ -83,11 +89,11 @@ def file_removeSymlinkedCopies(file:Path, l_ref:list=[]) -> int:
         count +=  1
     return count
 
-def file_process(this, file:Path) -> bool:
+def file_process(this, file:Path, onobjectstorage=False) -> bool:
     b_success:bool          = False
     b_symbolicLink:bool     = False
     file                    = this.path_expand(file)
-    df_file:pd.DataFrame    = LS.files_get(file)
+    df_file:pd.DataFrame    = LS.files_get(file, attribs = "", onObjectStorage = onobjectstorage)
     if df_file.empty:
         print(f"{file} not found... skipping.")
         return b_success
@@ -104,7 +110,7 @@ def file_process(this, file:Path) -> bool:
         # This is "real" file... delete it from its
         # directory manifest and remove the real from
         # from the real FS.
-        FILE.File(file).manifest.remove_entry('name', file.name)
+        FILE.File(file, onobjectstorage).manifest.remove_entry('name', file.name)
         b_success = file_rm(this, file)
 
     return b_success
@@ -112,18 +118,36 @@ def file_process(this, file:Path) -> bool:
 def singleElement_generator(path:Path) -> Generator[Path, None, None]:
      yield path
 
-def delete_do(this, src:Path, show:bool) -> int:
+def df_generator(df:pd.DataFrame) -> Generator[pd.Series, Any, None]:
+    for _, row in df.iterrows():
+        yield row
+
+def wildcards_check(path:Path) -> tuple[Path, str]:
+    wildcard:str    = ''
+    if '*' in path.name:
+        wildcard    = path.name
+        path        = path.parent
+    return path, wildcard
+
+def source_resolve(src:Path) -> Path:
+    wildcard:str            = ""
+    base:Path               = Path('')
+    base, wildcard          = wildcards_check(src)
+    if wildcard:
+        src                 = base
+    else:
+        src                 = base.parent
+    return src
+
+def delete_do(this, src:Path, show:bool, onobjectstorage:bool) -> int:
     count:int  = 0
-    iterator:Generator[Path, None, None] = singleElement_generator(src)
-    if "*" in src.name:
-         iterator = src.parent.expanduser().glob(src.name)
-    elif src.is_dir():
-         iterator = src.iterdir()
+    iterator:Generator[pd.Series, Any, None]   = df_generator(LS.files_get(src))
+    src                     = source_resolve(src)
     for child in iterator:
-        # if child.is_file():
+        childFile:Path      = src / Path(child['name'])
         if show:
             print(f"[rm]{str(src):50}")
-        file_process(this, child)
+        file_process(this, childFile, onobjectstorage)
         count += 1
     return count
 
@@ -140,15 +164,18 @@ of real data deletion, all linked files are also removed.
 
 """)
 @click.argument('source', required = True)
+@click.option('--onobjectstorage',
+              is_flag   = True,
+              help      = 'If set, perform operation on objectStorage')
 @click.option('--recursive',
               is_flag = True,
               help    = 'If set, do a recursive delete [not implemented]')
 @click.option('--show',
               is_flag = True,
               help    = 'If set, print the files as they are deleted')
-def rm(source, show, recursive) -> int:
-    # pudb.set_trace()
+def rm(source, show, onobjectstorage, recursive) -> int:
+    # c()
     Rm:_Rm              = _Rm()
     src:Path            = Path(source)
-    filesDeleted:int    = Rm.delete_do(src, show)
+    filesDeleted:int    = Rm.delete_do(src, show, onobjectstorage)
     return filesDeleted
