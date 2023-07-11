618fca86b0a941d1bc472517602bafda2e0bf82b
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-07-11 09:41:23 -0400

    Add bulk ops

--- commands/ls.py
+++ commands/ls.py
@@ -6,20 +6,49 @@ import  os
 import  pandas      as      pd
 import  pudb
 from    tabulate    import  tabulate
+from    typing      import  Callable, Pattern
+import  fnmatch
+import  re
 
 _ls:type        = core.Core
+LS:_ls  = _ls()
+LS.init()
 
-def files_get(this, path:Path, attribs:str = "") -> pd.DataFrame:
+def wildcards_check(target:Path) -> tuple[Path, str]:
+    wildcard:str    = ''
+    if '*' in target.name:
+        wildcard    = target.name
+        target      = target.parent
+    return target, wildcard
+
+def files_get(this, path:Path, **kwargs) -> pd.DataFrame:
+    # pudb.set_trace()
+    attribs:str             = ""
+    onObjectStorage:bool    = False
+    filter:str              = ""
     df_files:pd.DataFrame   = pd.DataFrame()
     manifestFile:Path       = Path('')
     file:str                = ""
-    manifestFile, file      = this.manifest_get(path)
+
+    for k,v in kwargs.items():
+        if k == 'attribs'           :   attribs         = v
+        if k == 'onObjectStorage'   :   onObjectStorage = v
+
+    path, filter            = wildcards_check(path)
+    manifestFile, file      = this.manifest_get(path, onObjectStorage)
+    realfs:Callable         = this.cfs2fs
+    if onObjectStorage:
+        realfs              = this.cfs2ofs
     try:
-        df_files            = pd.read_csv(str(this.cfs2fs(manifestFile)))
+        df_files            = pd.read_csv(str(realfs(manifestFile)))
     except:
         pass
     if file:
         df_files            = df_files[df_files['name'] == file]
+    if filter:
+        regex_pattern:str   = fnmatch.translate(filter)
+        df_files            = df_files[df_files['name'].str.\
+                                contains(regex_pattern, na = False, regex = True)]
     if attribs:
         l_attribs:list      = attribs.split(',')
         df_files            = df_files[l_attribs]
@@ -28,7 +57,10 @@ def files_get(this, path:Path, attribs:str = "") -> pd.DataFrame:
 def files_print(this, df_files:pd.DataFrame, long:bool) -> bool:
     if df_files.empty:
         return False
-    df_sorted:pd.DataFrame = df_files.sort_values(['type', 'name'])
+    if 'type' in df_files.columns and 'name' in df_files.columns:
+        df_sorted:pd.DataFrame = df_files.sort_values(['type', 'name'])
+    else:
+        df_sorted:pd.DataFrame = df_files
     if not len(df_files):
         return False
     if long:
@@ -38,6 +70,14 @@ def files_print(this, df_files:pd.DataFrame, long:bool) -> bool:
             lambda row: row['name'] + '/' if row['type'] == 'dir' else row['name'], axis=1)
         print(tabulate(df_sorted[['name']], showindex = False, tablefmt = "plain"))
     return True
+
+def path_checkAndExpand(path:str) -> Path:
+    pathChecked:Path    = Path('')
+    if path:
+        pathChecked     = LS.path_expand(Path(path))
+    else:
+        pathChecked     = LS.cwd()
+    return pathChecked
 
 _ls.files_get       = files_get
 
@@ -60,21 +100,26 @@ such a case CUBE is returning the file references as if they were real actual
 files.
 
 """)
-@click.argument('path',     required = False)
+@click.argument('path',
+                required = False)
+@click.option('--onobjectstorage',
+              is_flag   = True,
+              help      = 'If set, perform operation on objectStorage')
 @click.option('--attribs',  required = False,
               help      = 'A comma separated list of file attributes to return/print')
 @click.option('--long',
               is_flag   = True,
               help      = 'If set, use a long listing format')
-def ls(path:str, attribs:str, long) -> None:
+def ls(path:str, attribs:str, long, onobjectstorage) -> None:
     # pudb.set_trace()
-    LS:_ls  = _ls()
-    LS.init()
+    target:Path     = Path('')
     if path:
-        path   = LS.path_expand(Path(path))
-    else:
-        path   = LS.cwd()
-    files_print(LS, files_get(LS, Path(path), attribs), long)
+        target = Path(path)
+    files_print(LS,
+                files_get(LS, target,
+                              attribs           = attribs,
+                              onObjectStorage   = onobjectstorage),
+                long)
     print("")
 
 
