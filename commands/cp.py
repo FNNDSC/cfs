e8bcf450a7dea940acf0fb1b1678647c66443835
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2023-07-11 09:42:23 -0400

    Add bulk ops

--- commands/cp.py
+++ commands/cp.py
@@ -4,6 +4,7 @@ from    lib         import  core
 from    lib         import  file    as FILE
 from    commands    import  imp, ls
 from    pathlib     import  Path
+from    typing      import  Generator, Any
 import  pandas      as      pd
 import  pudb
 
@@ -12,10 +13,16 @@ LS:_ls          = _ls()
 
 _Cp:imp._Imp    = imp._Imp
 
-def manifest_refsAdd(path:Path, refmark:str, ref:Path, df_ref:pd.DataFrame=pd.DataFrame()) \
-    -> pd.DataFrame:
-    pathManifest:FILE.Manifest  = FILE.Manifest(FILE.File(path))
-    df_file:pd.DataFrame        = LS.files_get(path)
+def manifest_refsAdd(
+        path:Path,
+        refmark:str,
+        ref:Path,
+        df_ref:pd.DataFrame     = pd.DataFrame(),
+        onObjectStorage:bool    = False
+) -> pd.DataFrame:
+    # pudb.set_trace()
+    pathManifest:FILE.Manifest  = FILE.Manifest(FILE.File(path, onObjectStorage))
+    df_file:pd.DataFrame        = LS.files_get(path, onObjectStorage = onObjectStorage)
     b_manifestUpdate:bool       = False
     if df_file['refs'].isnull().values.any():
         df_file['refs']         = f'{refmark}{ref}'
@@ -32,51 +39,88 @@ def manifest_refsAdd(path:Path, refmark:str, ref:Path, df_ref:pd.DataFrame=pd.Da
         pathManifest.update_entry(df_file.to_dict(orient='records')[0])
     return df_file
 
-def file_copy(this, source:Path, target:Path) -> pd.DataFrame:
+def file_copy(this, source:Path, target:Path, fromObjectStorage:bool = False) -> pd.DataFrame:
     # pudb.set_trace()
     source              = this.path_expand(source)
     target              = this.path_expand(target)
-    sourceRefs:str      = FILE.File(source).refs
-    if sourceRefs:
-        try:
-            source      = Path(str(sourceRefs).split('<-')[1])
-        except:
-            pass
+    sourceRefs:str      = FILE.File(source, fromObjectStorage).refs
     if this.cfs2fs(target).is_dir():
         target          = target / Path(source.name)
-    return manifest_refsAdd(target,'<-', source,
-                            manifest_refsAdd(source, '->', target)
+    if sourceRefs:
+        try:
+            source, fromObjectStorage   = FILE.sourcePartition_resolve(
+                Path(str(sourceRefs).split('<-')[1])
+            )
+        except:
+            pass
+    return manifest_refsAdd(target,'<-obj:', source,
+                            manifest_refsAdd(
+                                source, '->', target,
+                                pd.DataFrame(),
+                                fromObjectStorage)
                             )
+def df_generator(df:pd.DataFrame) -> Generator[pd.Series, Any, None]:
+    for _, row in df.iterrows():
+        yield row
 
-# _Cp.manifest_resolve    = manifest_resolve
-# _Cp.source_update       = source_update
+def wildcards_check(path:Path) -> tuple[Path, str]:
+    wildcard:str    = ''
+    if '*' in path.name:
+        wildcard    = path.name
+        path        = path.parent
+    return path, wildcard
+
+def source_resolve(src:Path) -> Path:
+    wildcard:str            = ""
+    base:Path               = Path('')
+    base, wildcard          = wildcards_check(src)
+    if wildcard:
+        src                 = base
+    else:
+        src                 = base.parent
+    return src
+
+def copy_do(this, src:Path, dest:Path, show:bool = False) -> int:
+    # pudb.set_trace()
+    count:int               = 0
+    iterator:Generator[pd.Series, Any, None]   = df_generator(LS.files_get(src))
+    src                     = source_resolve(src)
+    for target in iterator:
+        sourceFile:Path     = src / Path(target['name'])
+        if show:
+            print(f"{sourceFile:50} -> [CFS]{dest}")
+        this.copy(src / Path(target['name']), dest)
+        count += 1
+    return count
+
+_Cp.copy                = file_copy
+_Cp.copy_do             = copy_do
 
 @click.command(help="""
                                 copy files
 
-This command "copies" files from one part of ChRIS storage to another. It is
-more akin to a "symbolic" link in standard UNIX file systems since data is
-not actually duplicated. Rather, meta-data is stored in the hidden _fstat
-file of the __CHRISOS folder in a given directory.
+This command "copies" files from one part of ChRIS "storage" to another. In reality, only
+single actual copy of the file exists in object storage, and the "copy" operation edits
+the database document at the target directory key.
+
+It is more akin to a "hard" link in standard UNIX file systems since data is not actually
+duplicated.
 
 """)
 @click.argument('source', required = True)
 @click.argument('target', required = True)
+@click.option('--show',
+              is_flag = True,
+              help    = 'If set, print the files as they are imported')
 @click.option('--recursive',
               is_flag=True,
               help='If set, do a recursive copy')
-def cp(source, target, recursive) -> None:
-    # pudb.set_trace()
+def cp(source, target, recursive, show) -> int:
     # Here we define the prototypical inheritance:
     # We create an instance of Cp that is essentially a
     # "clone" of the "imp"ort copy. Once instantiated,
     # we overwrite for this instance the file_copy method
     Cp:_Cp          = _Cp()
-    Cp.file_copy    = file_copy
+    filesCopied:int = Cp.copy_do(Path(source), Path(target), show)
 
-    Cp.file_copy(Path(source), Path(target))
-
-    if recursive:
-        print(f'Performing a recursive copy...')
-    else:
-        print(f'Performing a single copy...')
+    return filesCopied
