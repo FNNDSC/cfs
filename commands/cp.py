c725e5d950532dd3347f2780bf50933f2d70187b
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2023-07-11 21:14:07 -0400

    Add machinery for directory handling

--- commands/cp.py
+++ commands/cp.py
@@ -1,9 +1,9 @@
 import  click
 from    prototype   import  *
-from    lib         import  core
+from    lib         import  core, jobber
 from    lib         import  file    as FILE
 from    commands    import  imp, ls
-from    pathlib     import  Path
+from    pathlib     import  Path, PurePosixPath
 from    typing      import  Generator, Any
 import  pandas      as      pd
 import  pudb
@@ -70,31 +70,69 @@ def wildcards_check(path:Path) -> tuple[Path, str]:
         path        = path.parent
     return path, wildcard
 
-def source_resolve(src:Path) -> Path:
+def source_resolve(this, src:Path) -> Path:
     wildcard:str            = ""
     base:Path               = Path('')
     base, wildcard          = wildcards_check(src)
     if wildcard:
         src                 = base
     else:
-        src                 = base.parent
+        if this.cfs2fs(base).is_dir():
+            src             = base
+        else:
+            src             = base.parent
     return src
+
+def makedir(this, targetlist:list[Path]) -> list[Path]:
+    # pudb.set_trace()
+    system                  = jobber.Jobber({'verbosity': 0, 'noJobLogging': True})
+    for target in targetlist:
+        if this.cfs2fs(target).is_dir():
+            continue
+        command:str         = f"cfs mkdir {target}"
+        d_run:dict[Any, Any]= system.job_run(command)
+    return targetlist
+
+def target_dirsResolve(this, target:Path, descendents:list[Path]) -> list[Path]:
+    # pudb.set_trace()
+    targetlist:list         = []
+    targetlist              = [Path(target) / Path(d) for d in descendents]
+    return targetlist
+
+def descendentsOnly_get(this, src:Path, childDirs:list[Path]) -> list[Path]:
+    # pudb.set_trace()
+    dirlist:list[Path]      = [Path(str(d).replace(f"{str(src)}/", '')) for d in childDirs]
+    return dirlist
+
+def childDirs_get(this, src:Path) -> tuple[Path, list[Path]]:
+    # pudb.set_trace()
+    system                  = jobber.Jobber({'verbosity': 0, 'noJobLogging': True})
+    command:str             = f"cfs tree --mask {this.dbRoot} --filter {this.core.metaDir} {src}"
+    d_run:dict[Any, Any]    = system.job_run(command)
+    dirlist:list            = this.dirlist_resolve(d_run)
+    return src, dirlist
 
 def copy_do(this, src:Path, dest:Path, show:bool = False) -> int:
     # pudb.set_trace()
     count:int               = 0
     iterator:Generator[pd.Series, Any, None]   = df_generator(LS.files_get(src))
-    src                     = source_resolve(src)
+    src                     = source_resolve(this, src)
     for target in iterator:
         sourceFile:Path     = src / Path(target['name'])
+        if this.cfs2fs(sourceFile).is_dir():
+            continue
         if show:
             print(f"{sourceFile:50} -> [CFS]{dest}")
         this.copy(src / Path(target['name']), dest)
         count += 1
     return count
 
-_Cp.copy                = file_copy
-_Cp.copy_do             = copy_do
+_Cp.copy                    = file_copy
+_Cp.copy_do                 = copy_do
+_Cp.childDirs_get           = childDirs_get
+_Cp.descendentsOnly_get     = descendentsOnly_get
+_Cp.target_dirsResolve      = target_dirsResolve
+_Cp.makedir                 = makedir
 
 @click.command(help="""
                                 copy files
@@ -110,17 +148,30 @@ duplicated.
 @click.argument('source', required = True)
 @click.argument('target', required = True)
 @click.option('--show',
-              is_flag = True,
-              help    = 'If set, print the files as they are imported')
+              is_flag   = True,
+              help      = 'If set, print the files as they are imported')
 @click.option('--recursive',
-              is_flag=True,
-              help='If set, do a recursive copy')
-def cp(source, target, recursive, show) -> int:
-    # Here we define the prototypical inheritance:
-    # We create an instance of Cp that is essentially a
-    # "clone" of the "imp"ort copy. Once instantiated,
-    # we overwrite for this instance the file_copy method
-    Cp:_Cp          = _Cp()
-    filesCopied:int = Cp.copy_do(Path(source), Path(target), show)
+              is_flag   = True,
+              help      = 'If set, do a recursive copy')
+def cp(source:str, target:str, recursive:bool, show:bool) -> int:
+    Cp:_Cp                  = _Cp()
+    pudb.set_trace()
+    childDirs:list[Path]    = []
+    targetDirs:list[Path]   = []
+    sourceDir:Path          = Path('')
+    if recursive:
+        sourceDir, childDirs    = Cp.childDirs_get(Path(source))
+        targetDirs              = Cp.target_dirsResolve(Path(target),
+                                    Cp.descendentsOnly_get(sourceDir, childDirs)
+                                )
+    if targetDirs:
+        Cp.makedir(targetDirs)
+    else:
+        targetDirs.append(Path(target))
+    if not childDirs:
+        childDirs.append(Path(source))
+    filesCopied:int         = 0
+    for src, dest in zip(childDirs, targetDirs):
+        filesCopied        += Cp.copy_do(Path(src), Path(dest), show)
 
     return filesCopied
